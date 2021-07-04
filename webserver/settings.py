7f89b747cb0ebe006dfe8bc6940e58d183cb87a0
David Kim <jk2537@cornell.edu>
2021-07-04 03:00:57 -0400

    Debug off

--- webserver/settings.py
+++ webserver/settings.py
@@ -25,7 +25,7 @@ import os
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "bible-planner-backend.herokuapp.com", "127.0.0.1", "localhost"
