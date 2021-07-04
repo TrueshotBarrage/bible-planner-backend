c542fa1ef208c348aabf9885cc38254f8fba1fe4
David Kim <jk2537@cornell.edu>
2021-07-04 02:04:30 -0400

    Ready for production

--- webserver/settings.py
+++ webserver/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--yy=&ec6f3$n9zpfw0%(d2%epd2&3(lzwm25g^^=)^*z^fe=^^'
+import os
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "bible-planner-backend.herokuapp.com", "127.0.0.1", "localhost"
