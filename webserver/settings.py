ab7a2390c1b68869d78894b84255561a7200589e
David Kim <jk2537@cornell.edu>
2021-07-04 02:38:55 -0400

    Debug on again

--- webserver/settings.py
+++ webserver/settings.py
@@ -21,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 import os
+
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "bible-planner-backend.herokuapp.com", "127.0.0.1", "localhost"
